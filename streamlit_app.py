9382fb5e940e0fdd3465753e09739ab19c9eb850
anmedinar <123221229+anmedinar@users.noreply.github.com>
2023-01-21 17:41:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit;
 
-
 streamlit.title("My Parents New Healthy Diner");
 
 streamlit.header("Breakfast Menu");
 streamlit.text("Omega 3 & Blueberry Oatmeal");
 streamlit.text("kale, Spinach & Rocket Smoothie");
 streamlit.text("Hard-Boiled Free-Range Egg");
-
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 
@@ -22,20 +20,19 @@ fruits_to_show = my_fruit_list.loc[fruit_selected];
 #display the table on the page
 streamlit.dataframe(fruits_to_show);
 
-
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!");
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
-
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import snowlake.connector;
 
