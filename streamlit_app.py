a9d7f596d60cf1bb0e196d017c411a8c3d26f3a6
anmedinar <123221229+anmedinar@users.noreply.github.com>
2023-01-21 18:37:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ try:
      streamlit.error("Please select a fruit to get information.");
   else:
     back_from_function = get_fruitvice_data(fruit_choice)
-    #streamlit.dataframe(back_from_function);
+    streamlit.dataframe(back_from_function);
     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
     #streamlit.dataframe(fruityvice_normalized);
